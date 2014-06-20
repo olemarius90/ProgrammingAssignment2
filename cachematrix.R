0fa93de55ac6b0469e183903d93b6f0d2cbd6363
Ole Marius Hoel Rindal <olemarius@olemarius.net>
2014-06-20 09:35:08 +0200

    Caching the Inverse of a Matrix
    Two functions written for the programming assignment 2 in the coursera course "R programming". These functions calculates and caches the inverse of a matrix making a very computational efficient way of getting the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions computes the inverse of a matrix, but
+## caches the calculated inverse instead of computing it 
+## repeatedly. This saves computation time since the inverse
+## is only computed ones. If the matrix is reset, the
+## inverse of the new matrix is computed and cached.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function creates a special "matrix" 
+## object than can cahce its inverse. The matrix can be reset 
+## and then the inverse of the new matrix will be calculated 
+## once and cahced.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      invM <- NULL
+      set <- function(y) {
+        x <<- y
+        invMatrix <<- NULL
+      }
+      get <- function() x
+      setInvM <- function(invMatrix) invM <<- invMatrix
+      getInvM <- function() invM
+      list(set = set, get = get,
+           setInvM = setInvM,
+           getInvM = getInvM)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function takes a special "matrix" object as
+## an argument and checks if that matrix allready has its
+## inverse calculated. If the inverse is allready calculated
+## the cached inverse is returned. If the inverse is not
+## calculated this function calculates the inverse and
+## cahce the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      invM <- x$getInvM()
+      if(!is.null(invM)) {
+        message("getting cached data")
+        return(invM)
+      }
+      data <- x$get()
+      invM <- solve(data, ...)
+      x$setInvM(invM)
+      invM
 }
